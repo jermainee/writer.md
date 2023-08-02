e72b39e78008fc3406363607f3e92e87c526267d
Jermaine Jonas Ernst <34961982+jermainee@users.noreply.github.com>
2023-08-02 18:15:04 +0200

    Fetch response from openai generator; Render streamed markdown text

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import ReactMarkdown from 'react-markdown'
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+interface IAppState {
+    keyword: string|null;
+    isSubmitted: boolean;
+    markdownText: string;
 }
 
-export default App;
+export default class App extends Component<{}, IAppState> {
+    public constructor(props: {}) {
+        super(props);
+        this.state = {
+            keyword: null,
+            isSubmitted: false,
+            markdownText: '',
+        };
+    }
+    public render() {
+        if (!this.state.isSubmitted) {
+            return (
+                <div>
+                    <input onChange={this.handleInputChange} type="text" placeholder="What is the keyword?"/>
+                    <button onClick={this.handleButtonClick}>write</button>
+                </div>
+            );
+        }
+
+        return (
+            <ReactMarkdown>{this.state.markdownText}</ReactMarkdown>
+        );
+    }
+
+    private fetchWriterResponse = () => {
+        fetch(`http://127.0.0.1:5001/generate/${this.state.keyword}?openai_key=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`)
+            .then(async (response) => {
+                // response.body is a ReadableStream
+                // @ts-ignore
+                const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
+                for await (const chunk of this.readChunks(reader)) {
+                    this.setState({ markdownText: this.state.markdownText + chunk });
+                }
+            })
+            .catch((err) => console.error(err));
+    }
+
+    private handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ keyword: event.target.value });
+    }
+    private handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        console.log(this.state.keyword);
+        this.setState({ isSubmitted: true });
+
+        this.fetchWriterResponse();
+    }
+
+    private readChunks(reader: ReadableStreamDefaultReader) {
+        return {
+            async* [Symbol.asyncIterator]() {
+                let readResult = await reader.read();
+                while (!readResult.done) {
+                    yield readResult.value;
+                    readResult = await reader.read();
+                }
+            },
+        };
+    }
+}
